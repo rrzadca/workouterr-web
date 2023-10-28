72ce47a80ead8a95eaaf12f40fa8c7dcfff9def7
rrzadca <rrzadca@gmail.com>
2023-10-28 19:43:51 +0200

    fix default button styles

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -3,6 +3,7 @@ import {
     Component,
     Input,
     OnChanges,
+    OnInit,
     SimpleChanges,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -17,11 +18,27 @@ export type ButtonSize = 'sm' | 'md' | 'default' | 'lg' | 'xl';
     imports: [CommonModule],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent implements OnChanges {
+export class ButtonComponent implements OnChanges, OnInit {
     private cssClasses: string[] = [];
     @Input() size: ButtonSize = 'default';
 
+    ngOnInit(): void {
+        this.setCssClasses();
+    }
+
     ngOnChanges(changes: SimpleChanges): void {
+        if (
+            changes?.['size']?.previousValue !== changes?.['size']?.currentValue
+        ) {
+            this.setCssClasses();
+        }
+    }
+
+    get cssClass(): string {
+        return this.cssClasses.join(' ');
+    }
+
+    private setCssClasses(): void {
         this.cssClasses = [
             'font-semibold bg-lime-600 text-white shadow-sm  hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600',
         ];
@@ -43,9 +60,5 @@ export class ButtonComponent implements OnChanges {
                 this.cssClasses.push('rounded-md px-3.5 py-2.5 text-sm');
                 break;
         }
-    }
-
-    get cssClass(): string {
-        return this.cssClasses.join(' ');
     }
 }
