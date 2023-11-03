5530a310dd80e4bac9a8b8fdc004d530c85819ec
rrzadca <rrzadca@gmail.com>
2023-11-03 14:37:07 +0100

    add input-text to doc view

--- src/app/views/doc/doc.component.ts
+++ src/app/views/doc/doc.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../components/button/button.component';
+import { InputTextComponent } from '../../components/inputs/input-text/input-text.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-doc',
     standalone: true,
-    imports: [CommonModule, ButtonComponent],
+    imports: [CommonModule, ButtonComponent, InputTextComponent, FormsModule],
     templateUrl: './doc.component.html',
     styleUrls: ['./doc.component.scss'],
 })
-export class DocComponent {}
+export class DocComponent {
+    textInput = '';
+}
